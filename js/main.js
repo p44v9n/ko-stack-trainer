d07d803573592ff3633c8547cd0e7fc46390ff4a
p44v9n <ppb@DESKTOP-D19K82G.home>
2017-02-23 02:18:32 +0000

    checking function on button press is working for question style a

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var viewModel = {
 	userCardSuit: ko.observable(),
 	currentBank: ko.observableArray([1, 2, 3, 4, 5, 6, 7]),
 	currentCorrect: ko.observable(),
+	randomSeed:ko.observable(),
 
 	welcomeVisible: ko.observable(true),
 	appVisible:ko.observable(false),
@@ -45,7 +46,8 @@ var createNewStack = function(){
 // k denotes if last 3 / 5 / 10 were in correct in a row (k <= j)
 // (so that if we get a card wrong a bunch of times but then get it 3 (or 5 or 10) times correct in a row, we can move on)
 var scores = [];
-for (var i = 0; i<53; i++){
+scores.push(null);
+for (var i = 1; i<53; i++){
 	scores.push([0,0,0]);
 }
 
@@ -55,69 +57,6 @@ var begin = function(){
 	newQuestion();
 }
 
-var newQuestion = function(){
-	viewModel.currentCorrect = false;
-	// take card from from of array
-	if (viewModel.currentBank().length > 0) {
-		var x = viewModel.currentBank.shift();
-		viewModel.currentCardPosition(x);
-		viewModel.currentCardValue(stack[x][0]);
-		viewModel.currentCardSuit(stack[x][1]);
-		// do the question and add scores
-		var randomSeed = Math.floor((Math.random() * 10) + 1);
-		var randomSeed = 1; // for debug
-		switch (randomSeed) {
-			case 1: 
-				viewModel.questionStyleAVisible(true);
-
-			break;
-			case 2:
-				viewModel.questionStyleBVisible(true);
-				// do the getting user input here
-				// then
-				
-			break;
-			case 3:
-			break;
-			case 4:
-			break;
-		}
-		viewModel.nextVisible(false);
-		viewModel.checkVisible(true);
-}
-
-var check = function(){
-
-		// scorekeeping:
-		scores[x][0]++; // increase attempts by 1
-		if (viewModel.currentCorrect()) { // if correct add correct answers by 1 and streak of corrects
-			scores[x][1]++;
-			scores[x][2]++;
-			// disableButtons and show green on selected
-
-		} else { // else reset streak of corrects
-			scores[x][2] = 0;
-			// disableButtons, show green on correct and red on the selected
-		}
-
-
-		if (scores[x][2]>5) { // if streak > 5
-			// add new card (number) to bank and don't re add current card
-		} else { // else readd
-			viewModel.currentBank.push(x);
-		}
-		// if scores[x][2] > 5 then {
-			// dont readd card to bank and instead the next one along that has scores[x][0] === 0 (i.e. has no attempts)
-		// } else {
-		// put back in bank at other end i.e. so that when we pop we pick a new card
-		// }
-		// if 
-
-		viewModel.checkVisible(false);
-		viewModel.nextVisible(true);
-	}
-}
-
 
 // styles of question:
 // a what number is the 4C?
@@ -125,6 +64,95 @@ var check = function(){
 // c what comes before the 4C?
 // d what comes after the 4C
 
+
+var newQuestion = function(){
+	console.log("bank:"+viewModel.currentBank());
+	// todo: remove all selected correct wrong classes
+	// reset current 
+	viewModel.userCardSuit(null);
+	viewModel.userCardValue(null);
+	viewModel.userCardPosition(null);
+	viewModel.currentCorrect(false);
+	// take card from from of array
+	if (viewModel.currentBank().length > 0) {
+		var x = viewModel.currentBank.shift();
+		viewModel.currentCardPosition(x);
+		viewModel.currentCardValue(stack[x][0]);
+		viewModel.currentCardSuit(stack[x][1]);
+		// do the question and add scores
+		viewModel.randomSeed = Math.floor((Math.random() * 10) + 1);
+		viewModel.randomSeed = 1; // for debug
+		switch (viewModel.randomSeed) {
+			case 1: // user picks position of given card
+				viewModel.questionStyleAVisible(true);
+
+			break;
+			case 2: // user picks card at given position 
+				viewModel.questionStyleBVisible(true);		
+			break;
+			case 3: // user gives card before a given card
+			break;
+			case 4: // user gives card that is after a given card
+			break;
+		}
+		viewModel.nextVisible(false);
+		viewModel.checkVisible(true);
+	}
+}
+
+var check = function(){
+
+	$('.positiondiv').prop('disabled', true); // disable all buttons
+
+	switch (viewModel.randomSeed) {		
+		case 1: // user picks position of given card
+			if (viewModel.userCardPosition === viewModel.currentCardPosition) {
+				viewModel.currentCorrect(true);
+			}
+		break;
+		case 2: // user picks card at given position 
+		break;
+		case 3: // user gives card before a given card
+		break;
+		case 4: // user gives card that is after a given card
+		break;
+	}
+
+	var x = viewModel.currentCardPosition();
+	// scorekeeping:
+	scores[x][0]++; // increase attempts by 1
+	if (viewModel.currentCorrect()) { // if correct add correct answers by 1 and streak of corrects
+		console.log("correct");
+		scores[x][1]++;
+		scores[x][2]++;
+		$('.selected').addClass('correct');
+
+	} else { // else reset streak of corrects
+		console.log("wrong");
+		scores[x][2] = 0;
+		// show green on correct and red on the selected
+		$('.selected').addClass('wrong');
+
+	}
+
+	// todo check bank being updated
+	if (scores[x][2]>5) { // if streak > 5
+		// add new card (number) to bank and don't re add current card
+	} else { // else readd
+		viewModel.currentBank.push(x);
+	}
+	// if scores[x][2] > 5 then {
+		// dont readd card to bank and instead the next one along that has scores[x][0] === 0 (i.e. has no attempts)
+	// } else {
+	// put back in bank at other end i.e. so that when we pop we pick a new card
+	// }
+	// if 
+	viewModel.checkVisible(false);
+	viewModel.nextVisible(true);
+}
+
+
+// below is obsolete 
 
 var newCorrectAnswer = function(number) {
 	for (var i = 0; i<3; i++){
